96b04944a095f4186466da921804a1a781a1a88d
Jared Lunde <jared.lunde@gmail.com>
2024-09-29 11:54:54 -0700

    add download

--- lib/utils.bash
+++ lib/utils.bash
@@ -93,8 +93,7 @@ install_version() {
 		mkdir -p "$install_path"
 		cp -r "$ASDF_DOWNLOAD_PATH"/* "$install_path"
 
-		local tool_cmd
-		tool_cmd="$(echo "$TOOL_TEST" | cut -d' ' -f1)"
+		tool_cmd=envtpl
 		test -x "$install_path/$tool_cmd" || fail "Expected $install_path/$tool_cmd to be executable."
 
 		echo "$TOOL_NAME $version installation was successful!"
